d80c3924e257149f90856559e05dae505a45f086
Alex.Bezbozhnov <alex.bezbozhnov@gmail.com>
2023-11-24 10:50:54 +0600

    создание папок

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:typed_data';
+import 'package:path/path.dart' as path; // Импорт библиотеки path
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
-import 'dart:convert';
 import 'file_view_page.dart';
 
 class MainPage extends StatefulWidget {
@@ -16,7 +17,30 @@ class _MainPageState extends State<MainPage> {
   @override
   void initState() {
     super.initState();
-    futureFiles = fetchUploadedFiles(); // Загрузка списка файлов
+    futureFiles = fetchUploadedFiles();
+  }
+
+  Future<void> downloadAndShowFileContents(String fileName) async {
+    final FirebaseStorage storage = FirebaseStorage.instance;
+    Reference reference = storage.ref().child('uploads/$fileName');
+
+    try {
+      File localFile =
+          File('${(await getTemporaryDirectory()).path}/$fileName');
+      await reference.writeToFile(localFile);
+
+      String contents = await localFile.readAsString();
+
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) =>
+              FileViewPage(fileName: fileName, fileContents: contents),
+        ),
+      );
+    } catch (error) {
+      print('Ошибка при загрузке или чтении файла: $error');
+    }
   }
 
   Future<List<String>> fetchUploadedFiles() async {
@@ -25,32 +49,35 @@ class _MainPageState extends State<MainPage> {
 
     try {
       ListResult result = await reference.listAll();
-      List<String> files = result.items.map((item) => item.name).toList();
-      return files;
+      List<String> items = result.items.map((item) => item.name).toList();
+      List<String> prefixes =
+          result.prefixes.map((prefix) => prefix.fullPath).toList();
+
+      List<String> filesAndFolders = [];
+      filesAndFolders.addAll(items);
+      filesAndFolders.addAll(prefixes);
+
+      return filesAndFolders;
     } catch (error) {
       print('Ошибка при получении файлов из Firebase Storage: $error');
       throw error;
     }
   }
 
-  Future<void> downloadAndShowFileContents(String fileName) async {
+  Future<void> createFolder(String folderName) async {
     final FirebaseStorage storage = FirebaseStorage.instance;
-    Reference reference = storage.ref().child('uploads/$fileName');
+    Reference reference = storage.ref().child('uploads/$folderName/');
 
     try {
-      File localFile = File('${(await getTemporaryDirectory()).path}/$fileName');
-      await reference.writeToFile(localFile);
+      // Создаем файл внутри папки, это создаст папку в Firebase Storage
+      await reference.child('/.keep').putData(Uint8List(0));
 
-      String contents = await localFile.readAsString();
-
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => FileViewPage(fileName: fileName, fileContents: contents),
-        ),
-      );
+      setState(() {
+        futureFiles =
+            fetchUploadedFiles(); // Обновление списка после создания папки
+      });
     } catch (error) {
-      print('Ошибка при загрузке или чтении файла: $error');
+      print('Ошибка при создании папки: $error');
     }
   }
 
@@ -81,17 +108,64 @@ class _MainPageState extends State<MainPage> {
               return ListView.builder(
                 itemCount: snapshot.data!.length,
                 itemBuilder: (context, index) {
+                  String itemName = snapshot.data![index];
+                  bool isFolder = itemName.endsWith('/');
+
+                  String fileName = path
+                      .basename(itemName); // Получаем только имя файла без пути
+
                   return ListTile(
-                    title: Text(snapshot.data![index]),
+                    title: Text(fileName), // Отображаем только имя файла
                     onTap: () {
-                      downloadAndShowFileContents(snapshot.data![index]);
+                      if (isFolder) {
+                        print('Вы выбрали папку: $itemName');
+                      } else {
+                        downloadAndShowFileContents(itemName);
+                      }
                     },
+                    leading: isFolder ? Icon(Icons.folder) : null,
                   );
                 },
               );
             }
           },
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              String newFolderName = ''; // Хранение имени новой папки
+
+              return AlertDialog(
+                title: Text('Новая папка'),
+                content: TextField(
+                  onChanged: (value) {
+                    newFolderName = value; // Сохранение введенного значения
+                  },
+                  decoration: InputDecoration(hintText: 'Введите имя папки'),
+                ),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('Отмена'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      createFolder(newFolderName); // Создание новой папки
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('Создать'),
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
