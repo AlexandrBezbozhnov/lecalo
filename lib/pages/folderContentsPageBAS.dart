0b05db92a1c5524a32dd019fb1f2dd1845d15e2c
Alex.Bezbozhnov <alex.bezbozhnov@gmail.com>
2023-11-27 15:18:31 +0600

    .

--- lib/pages/folderContentsPageBAS.dart
+++ lib/pages/folderContentsPageBAS.dart
@@ -4,17 +4,30 @@ import 'package:path_provider/path_provider.dart';
 import 'dart:io';
 import 'fileViewPage.dart';
 
-class FolderContentsPageBAS extends StatelessWidget {
+class FolderContentsPageBAS extends StatefulWidget {
   final String folderName;
   final List<String> contents;
 
   const FolderContentsPageBAS({
-      Key? key,
+    Key? key,
     required this.folderName,
     required this.contents,
   }) : super(key: key);
 
-  Future<String> _loadFileContents(String filePath) async {
+  @override
+  _FolderContentsPageBASState createState() => _FolderContentsPageBASState();
+}
+
+class _FolderContentsPageBASState extends State<FolderContentsPageBAS> {
+  List<String> _contents = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _contents = widget.contents;
+  }
+
+Future<String> _loadFileContents(String filePath) async {
     try {
       final ref = FirebaseStorage.instance.ref().child(filePath);
       final downloadUrl = await ref.getDownloadURL();
@@ -47,38 +60,48 @@ class FolderContentsPageBAS extends StatelessWidget {
     }
   }
 
+  Future<void> _deleteFile(String filePath) async {
+    try {
+      final ref = FirebaseStorage.instance.ref().child(filePath);
+      await ref.delete();
+      print('File deleted successfully');
+      setState(() {
+        _contents.remove(filePath); // удаление файла из списка
+      });
+    } catch (e) {
+      print("Error deleting file: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('$folderName'),
+        title: Text(widget.folderName),
       ),
       body: _buildContentsList(context),
     );
   }
 
   Widget _buildContentsList(BuildContext context) {
-    return contents.isEmpty
+    return _contents.isEmpty
         ? Center(child: Text('Папка пуста'))
         : ListView.builder(
-            itemCount: contents.length,
+            itemCount: _contents.length,
             itemBuilder: (context, index) {
-              String itemName = contents[index];
+              String itemName = _contents[index];
 
-              // Проверка на расширение файла .keep и пропуск его отображения
               if (itemName.endsWith('.keep') || itemName.endsWith('.txt')) {
-                return SizedBox.shrink(); // Пропуск отображения файла .keep или .bas
+                return SizedBox.shrink();
               }
 
               String fileName = itemName.split('/').last;
-              // Удаление расширения .txt из имени файла
               if (fileName.endsWith('.bas')) {
                 fileName = fileName.substring(0, fileName.length - 4);
               }
 
               return ListTile(
-                title: Text(
-                    fileName), // Отображение имени файла без расширения .txt
+                title: Text(fileName),
                 onTap: () async {
                   String fileContents = await _loadFileContents(itemName);
                   Navigator.push(
@@ -89,6 +112,35 @@ class FolderContentsPageBAS extends StatelessWidget {
                         fileContents: fileContents,
                       ),
                     ),
+                  );
+                },
+                onLongPress: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text("Удалить файл?"),
+                        content: Text("Вы уверены, что хотите удалить файл $fileName?"),
+                        actions: <Widget>[
+                          TextButton(
+                            child: Text("Отмена"),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                          TextButton(
+                            child: Text(
+                              "Удалить",
+                              style: TextStyle(color: Colors.red),
+                            ),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                              _deleteFile(itemName);
+                            },
+                          ),
+                        ],
+                      );
+                    },
                   );
                 },
               );
